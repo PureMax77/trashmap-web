1057f4cafb01cdd63bc1410039bc47454477e56d
PureMax <ertr777@naver.com>
2021-10-07 17:54:47 +0900

    fix video radius

--- src/components/PreVideo.js
+++ src/components/PreVideo.js
@@ -2,12 +2,6 @@ import React from "react"
 import styled from "styled-components"
 import ReactPlayer from "react-player/lazy"
 
-const Video = styled(ReactPlayer)`
-  overflow: hidden;
-  border: 2px solid rgb(232, 232, 232);
-  box-shadow: rgb(0 0 0 / 16%) 0px 3px 6px 0px;
-  border-radius: 20px;
-`
 const VideoWrap = styled.div`
   margin-bottom: ${(props) => props.marginBottom};
 
@@ -19,10 +13,23 @@ const VideoWrap = styled.div`
   }
 `
 
+const videoStyle = {
+  overflow: "hidden",
+  border: "2px solid rgb(232, 232, 232)",
+  boxShadow: "rgb(0 0 0 / 16%) 0px 3px 6px 0px",
+  borderRadius: "20px",
+}
+
 function PreVideo({ video, marginBottom }) {
   return (
     <VideoWrap marginBottom={marginBottom}>
-      <Video width={"100%"} height={"270px"} controls={true} url={video?.url} />
+      <ReactPlayer
+        width={"100%"}
+        height={"270px"}
+        controls={true}
+        url={video?.url}
+        style={videoStyle}
+      />
     </VideoWrap>
   )
 }
